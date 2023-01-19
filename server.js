9d71e09226e3fbc133312cf315506d9898d059c3
Adithya <m.adtihya777@gmail.com>
2023-01-20 00:05:26 +0530

    Dockerised app

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const calculator = require('./calculator');
 const express = require('express');
 const app = express();
 
+const PORT = 8080
+
 app.get('/', (req, res) => {
 	console.log('Adding 2 and 5, we get: ', calculator.add("2,3"))
 	res.send('Server is up!');
 });
 
-app.listen(3000, () => {
-	console.log('Server is listening on port 3000');
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
 });
