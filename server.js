baf4c0a953c0f9e7b71d375f66d18701518c3a6e
Adithya <adithya.m@harness.io>
2023-01-25 13:03:18 +0530

    PR checking

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Server is up!");
 });
 
+app.get('/adi-k8-cd', (req, res) => {
+  res.send("Yay, the final CD is working!");
+});
+
 app.get('/final-cd', (req, res) => {
   res.send("This is the final setup!");
 });
